2c8dcda6164badd70d55c114e4b6bf2d1cab2f3a
root <root@Miguel-Laptop.localdomain>
2018-04-23 12:26:22 +0100

    improved server features and autodisconnect system

---
+++ server/README.md
@@ -0,0 +1,3 @@
+For debugging server
+Prompt npm run debug
+On vs code hit F5 for starting debug for websocket
--- server/websocket.js
+++ server/websocket.js
@@ -23,25 +23,77 @@ var server = https.createServer(options, function (req, res) {
 });
 
 const wss = new WebSocket.Server({ server });
+// prototype client id and socket
+var Client = function(id, ws) {
+  this.id = id,
+  this.ws = ws
+};
+// full list of clients connected
+var clientSet = [];
+
+function noop() {}
+// a keep alive check
+function heartbeat() {
+  this.isAlive = true;
+}
 
 wss.on('connection', function connection(ws, req) {
   
   const ip = req.connection.remoteAddress;
   var client_uuid = uuid.v1();
+  console.log('%s new client connected %s', Date.now(), client_uuid);
+
+  ws.isAlive = true;
+  clientSet.push(new Client(client_uuid, ws));
 
   ws.send(
     JSON.stringify({ id: client_uuid })
   );
 
+  wss.broadcast(JSON.stringify({id: client_uuid, event:'new client connected'}));
+
   ws.on('message', function incoming(message) {
     try {
+      heartbeat();
       var msg = JSON.parse(message);
-      console.log('client id %s, action %s, data %s', msg.id, msg.action, msg.data);
+      wss.broadcast(JSON.stringify({id: msg.id, event:'action', 
+      action: msg.action, data: msg.data}));
     } catch(e) {
       console.error('message cannot be parsed '+message)
     }
   });
 
 });
+
+// Broadcast to all except the client who generate the event.
+wss.broadcast = function broadcast(data) {
+  try {
+    var message = JSON.parse(data);
+    clientSet.forEach(function(client){
+      if (client.id != message.id &&
+        client.ws.readyState === WebSocket.OPEN) {
+          client.ws.send(data);
+      }
+    });
+  } catch(e){
+    console.error(e);
+  }
+};
+
+const interval = setInterval(function ping() {
+  clientSet = clientSet.filter(function(client) {
+    if (client.ws.isAlive === false && client.ws.readyState !== WebSocket.OPEN) {
+      console.log("client id %s terminated", client.id);
+      client.ws.terminate();
+      return false;
+    } else {
+      client.ws.isAlive = false;
+      client.ws.ping(noop);
+      return true;
+    }
+  })
+  console.log(clientSet);
+}, 30000);
+
 
 server.listen(443);
