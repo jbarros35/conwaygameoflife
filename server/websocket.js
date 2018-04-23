254dcb1f2e697de5b817c8a462d0bf07b1a5c908
root <root@Miguel-Laptop.localdomain>
2018-04-23 14:58:43 +0100

    tested on firefox and chrome websockets running ok

--- server/websocket.js
+++ server/websocket.js
@@ -24,45 +24,55 @@ var server = https.createServer(options, function (req, res) {
 
 const wss = new WebSocket.Server({ server });
 // prototype client id and socket
-var Client = function(id, ws) {
+var Client = function(id, ws, date) {
   this.id = id,
-  this.ws = ws
+  this.ws = ws,
+  this.date = date,
+  this.lastActivity = date
 };
+
 // full list of clients connected
 var clientSet = [];
-
-function noop() {}
-// a keep alive check
-function heartbeat() {
-  this.isAlive = true;
-}
 
 wss.on('connection', function connection(ws, req) {
   
   const ip = req.connection.remoteAddress;
   var client_uuid = uuid.v1();
-  console.log('%s new client connected %s', Date.now(), client_uuid);
+  console.log('%s new client connected %s', new Date(), client_uuid);
 
   ws.isAlive = true;
-  clientSet.push(new Client(client_uuid, ws));
-
+  clientSet.push(new Client(client_uuid, ws, new Date()));
+  // send client id
   ws.send(
-    JSON.stringify({ id: client_uuid })
+    JSON.stringify({ id: client_uuid, event: 'client_id'})
   );
-
-  wss.broadcast(JSON.stringify({id: client_uuid, event:'new client connected'}));
+  // broadcast on connect new client
+  wss.broadcast(JSON.stringify(
+    {id: client_uuid, 
+      data: client_uuid,
+      date: new Date(),
+      event:'client_connected'}));
 
   ws.on('message', function incoming(message) {
     try {
-      heartbeat();
       var msg = JSON.parse(message);
-      wss.broadcast(JSON.stringify({id: msg.id, event:'action', 
-      action: msg.action, data: msg.data}));
+      wss.broadcast(JSON.stringify(
+        {id: msg.id, 
+          event:'action', 
+          action: msg.action, 
+          data: msg.data,
+          date: new Date()
+        }));
+      // update client activity log
+      var client = getClientId(msg.id);
+      if (client) {
+        client.lastActivity = new Date();
+        client.isAlive = true;
+      }
     } catch(e) {
       console.error('message cannot be parsed '+message)
     }
   });
-
 });
 
 // Broadcast to all except the client who generate the event.
@@ -80,20 +90,45 @@ wss.broadcast = function broadcast(data) {
   }
 };
 
+function getClientId(id) {
+  return clientSet.filter(function(client){
+    if (client.id === id) {
+      return client;
+    }
+  });
+};
+
+function getTimeElapsed(date) {
+  var today = new Date();
+  var diffMs = (today - date); // milliseconds between now & Christmas
+  var diffDays = Math.floor(diffMs / 86400000); // days
+  var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+  var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+  return {ms: diffMs, days: diffDays, hours: diffHrs, minutes: diffMins};
+};
+
 const interval = setInterval(function ping() {
+  var disconnected = [];
   clientSet = clientSet.filter(function(client) {
-    if (client.ws.isAlive === false && client.ws.readyState !== WebSocket.OPEN) {
-      console.log("client id %s terminated", client.id);
+    var lastActivity = getTimeElapsed(client.lastActivity);
+    if (client.ws.isAlive === false 
+      && client.ws.readyState !== WebSocket.OPEN) {
+      console.log("client id %s terminated last activity: %s", client.id, lastActivity.ms);
       client.ws.terminate();
+      disconnected.push(client.id);
       return false;
     } else {
       client.ws.isAlive = false;
-      client.ws.ping(noop);
+      console.log("client id %s is online last activity: %s", client.id, lastActivity.ms);
       return true;
     }
-  })
-  console.log(clientSet);
-}, 30000);
+  });
 
+  // tell all clients that someone disconnected
+  disconnected.forEach(function(id){
+    wss.broadcast(JSON.stringify({id: id, event:'disconnected'}));
+  });
+  
+}, 30000);
 
 server.listen(443);
